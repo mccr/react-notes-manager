818b407188e5a2e6506a4417d6fda934357a29a3
Monica Calderaro <mcalderarorey@gmail.com>
2023-08-22 12:32:57 +0200

    add noteapi fetch notes call

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "components/Header/index";
+import { setNoteList } from "store/notes/notes-slice";
+import { useDispatch } from "react-redux";
+import { NoteAPI } from "api/note-api";
+import { useEffect } from "react";
 
 export function App() {
+  const dispatch = useDispatch();
+
+  async function fetchNotes() {
+    const noteList = await NoteAPI.fetchAll();
+    dispatch(setNoteList(noteList));
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  });
+
   return (
     <div>
       <Header />
--- src/api/note-api.js
+++ src/api/note-api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const BASE_URL = "http:localhost:3200/notes";
 
-export class api {
+export class NoteAPI {
   static async create(note) {
     return (await axios.post(`${BASE_URL}`, note)).data;
   }
-  static async fetAll() {
+  static async fetchAll() {
     return (await axios.get(`${BASE_URL}`)).data;
   }
   static async fetchById(noteId) {
