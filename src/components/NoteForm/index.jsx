c93bf78a7030a46e93dd39c5b0ae67b995638c1c
Monica Calderaro <mcalderarorey@gmail.com>
2023-08-25 12:22:09 +0200

    Create new note and call api

--- src/components/NoteForm/index.jsx
+++ src/components/NoteForm/index.jsx
@@ -3,7 +3,13 @@ import style from "./style.module.css";
 import { ButtonPrimary } from "components/ButtonPrimary";
 import { useState } from "react";
 
-export const NoteForm = ({ title, onClickEdit, onClickDelete, onSubmit }) => {
+export const NoteForm = ({
+  title,
+  onClickEdit,
+  onClickDelete,
+  onSubmit,
+  isEditable,
+}) => {
   const [formValues, setFormValues] = useState({ title: "", content: "" });
 
   const updateFormValues = (e) => {
@@ -59,7 +65,7 @@ export const NoteForm = ({ title, onClickEdit, onClickDelete, onSubmit }) => {
     <div className={style.container}>
       <div className="row justify-content-space-between">
         <h1 className="mb-3 col-10">{title}</h1>
-        {actionIcons}
+        {isEditable && actionIcons}
       </div>
 
       <form className={style.form}>
--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,22 +1,29 @@
 import { NoteForm } from "components/NoteForm";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { NoteAPI } from "api/note-api";
 import { addNote } from "store/notes/notes-slice";
 
 export const NoteCreate = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const createNewNote = (formValues) => {
-    dispatch(
-      addNote({
-        title: formValues.title,
-        content: formValues.content,
-        created_at: new Date().toLocaleString(),
-      })
-    );
+  const createNewNote = async (formValues) => {
+    const createdNote = await NoteAPI.create({
+      title: formValues.title,
+      content: formValues.content,
+      created_at: new Date().toLocaleDateString(),
+    });
+    dispatch(addNote(createdNote));
+    navigate("/");
   };
   return (
     <>
-      <NoteForm title="Create note" onSubmit={createNewNote} />
+      <NoteForm
+        title="Create note"
+        onSubmit={createNewNote}
+        isEditable={false}
+      />
     </>
   );
 };
